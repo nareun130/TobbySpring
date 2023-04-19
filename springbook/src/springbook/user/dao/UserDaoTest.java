2f60f7557f314e450066b7e8ba7296adc948c4bb
nareun130 <nareun130@gmail.com>
2023-04-19 17:25:40 +0900

    [Feat] : xml설정 파일 추가
    @Configuration대신 applicationContext.xml파일 사용

--- springbook/src/springbook/user/dao/UserDaoTest.java
+++ springbook/src/springbook/user/dao/UserDaoTest.java
@@ -4,16 +4,19 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.domain.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		// xml을 사용하는 애플리케이션 컨텍스트
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 
 		User user = new User();
-		user.setId("whiteship");
+		user.setId("whiteship2");
 		user.setName("백기선");
 		user.setPassword("married");
 
