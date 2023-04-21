959102d48a1098c5b5bef46b41d079ccc7f0265b
nareun130 <nareun130@gmail.com>
2023-04-21 15:06:07 +0900

    [Feat] : 코드 수정
    User파라미터 생성자 추가,
    불필요한 클래스 삭제,
    주석 추가,
    count()테스트 메서드 추가

--- springbook/src/springbook/user/domain/User.java
+++ springbook/src/springbook/user/domain/User.java
@@ -1,16 +1,29 @@
 package springbook.user.domain;
 
-
 public class User {
 	String id;
 	String name;
 	String password;
+
+	public User() {
+		
+	}
+
+	//파라미터가 있는 User 생성자
+	public User(String id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
@@ -18,11 +31,13 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 }
