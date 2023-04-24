71ec9558844dbd653e659947783ec6d5944aa15b
nareun130 <nareun130@gmail.com>
2023-04-24 18:56:16 +0900

    [FEAT] : xml 의존주입
    xml에서 스트링 타입의 프로퍼티 값을 설정한 것이 빈에 잘 주입되었는지 테스트

--- springbook/src/springbook/user/dao/UserDaoTest3.java
+++ springbook/src/springbook/user/dao/UserDaoTest3.java
@@ -9,6 +9,7 @@ import javax.sql.DataSource;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,8 +22,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.domain.User;
 
-public class UserDaoTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/test-applicationContext.xml")
+public class UserDaoTest3 {
 
+	@Autowired
 	UserDao dao;
 
 	private User user1;
@@ -30,20 +34,14 @@ public class UserDaoTest {
 	private User user3;
 
 	@Before
-	public void setUp() {	//테스트 컨텍스트를 이용한 테스트에서 @Autowired로 가져온 빈 오브젝트
-		dao = new UserDao();
-		DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testdb", "spring", "book", true);
-		dao.setDataSource(dataSource);
-
+	public void setUp() {
 		this.user1 = new User("gyumee", "박성철", "springno1");
 		this.user2 = new User("leegw700", "이길원", "springno2");
 		this.user3 = new User("bumjin", "박범진", "springno3");
-
 	}
 
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
-
 		dao.deleteAll();
 		assertThat(dao.getCount(), is(0));
 
@@ -74,17 +72,6 @@ public class UserDaoTest {
 
 		dao.add(user3);
 		assertThat(dao.getCount(), is(3));
-	}
-
-	// expected를 추가함으로 예외가 발생하면 테스트가 성공함
-	@Test(expected = EmptyResultDataAccessException.class)
-	public void getUserFailure() throws SQLException, ClassNotFoundException {
-
-		dao.deleteAll();
-		assertThat(dao.getCount(), is(0));
-
-		dao.get("unknown_id"); // UserDao에 손을 대지 않아서 test가 실패
-
 	}
 
 	public static void main(String[] args) {
