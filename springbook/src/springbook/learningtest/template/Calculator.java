27c018b9463e26df28fe24663150e64f9730f2a3
nareun130 <nareun130@gmail.com>
2023-04-25 13:53:33 +0900

    [FEAT] : 제네릭을 이용한 lineReadTemplate
    제네릭스를 이용하여 Integer값과 String값 두개를 리턴 가능하도록 만들어 줬다.

--- springbook/src/springbook/learningtest/template/Calculator.java
+++ springbook/src/springbook/learningtest/template/Calculator.java
@@ -25,11 +25,11 @@ public class Calculator {
 		}
 	}
 
-	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
+	public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			Integer res = initVal;
+			T res = initVal;
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				res = callback.doSomethingWithLine(line, res);
@@ -51,7 +51,7 @@ public class Calculator {
 	}
 
 	public Integer calcSum(String filepath) throws IOException {
-		LineCallback sumCallback = new LineCallback() {
+		LineCallback<Integer> sumCallback = new LineCallback<Integer>() {
 
 			@Override
 			public Integer doSomethingWithLine(String line, Integer value) {
@@ -62,7 +62,7 @@ public class Calculator {
 	}
 
 	public Integer calcMultiply(String filepath) throws IOException {
-		LineCallback multiplyCallback = new LineCallback() {
+		LineCallback<Integer> multiplyCallback = new LineCallback<Integer>() {
 
 			@Override
 			public Integer doSomethingWithLine(String line, Integer value) {
@@ -70,5 +70,16 @@ public class Calculator {
 			}
 		};
 		return lineReadTemplate(filepath, multiplyCallback, 1);
+	}
+
+	public String concantenate(String filepath) throws IOException {
+		LineCallback<String> concatenateCallback = new LineCallback<String>() {
+
+			@Override
+			public String doSomethingWithLine(String line, String value) {
+				return value + line;
+			}
+		};
+		return lineReadTemplate(filepath, concatenateCallback, "");
 	}
 }
