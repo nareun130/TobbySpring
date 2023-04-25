f046ad213079d7760ab9703add3bb3864ac8e7d4
nareun130 <nareun130@gmail.com>
2023-04-25 14:32:56 +0900

    [FEAT] : JdbcContext를 JdbcTemplate으로 대체
    만든 JdbcContext클래스 대신 JdbcTemplate을 이용하여 쿼리를 처리

--- springbook/src/springbook/user/dao/UserDao.java
+++ springbook/src/springbook/user/dao/UserDao.java
@@ -1,99 +1,69 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
 
-	private DataSource dataSource;
-	private JdbcContext jdbcContext;
+	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
-		this.jdbcContext = new JdbcContext(); // jdbcContext를 생성
-		this.jdbcContext.setDataSource(dataSource); // 외존 오브젝트 주입
-		this.dataSource = dataSource;// 아직 jdbcContext를 적용하지 않은 메소드를 위해 저장해 둠.
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+
 	}
 
 	public void add(final User user) throws SQLException, ClassNotFoundException {
 
-		// 익명 내부 클래스를 메소드 파라미터로 이전
-		this.jdbcContext.executeSql("insert into users(id,name,password) values(?,?,?)", user.getId(), user.getName(),
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(),
 				user.getPassword());
 	}
 
-	public User get(String id) throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
+	public User get(String id) {
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] { id },
+				new RowMapper<User>() {
 
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
+					@Override
+					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+						User user = new User();
+						user.setId(rs.getString("id"));
+						user.setName(rs.getString("name"));
+						user.setPassword(rs.getString("password"));
+						return user;
 
-		ResultSet rs = ps.executeQuery();
+					}
 
-		User user = null;
-		// getUserFailure를 위한 코드 수정
-		if (rs.next()) {
-			user = new User();
-			user.setId(rs.getString("id"));
-			user.setName(rs.getString("name"));
-			user.setPassword(rs.getString("password"));
-		}
-
-		rs.close();
-		ps.close();
-		c.close();
-
-		if (user == null)
-			throw new EmptyResultDataAccessException(1);
-		return user;
+				});
 	}
 
 	public void deleteAll() throws SQLException {
-		this.jdbcContext.executeSql("delete from users");
+		this.jdbcTemplate.update("delete from users");
 	}
 
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
+	}
 
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (ps != null) {
-				try {
-					ps.close();
-				} catch (SQLException e) {
-				}
-			}
-			if (c != null) {
-				try {
-					c.close();
-				} catch (SQLException e) {
-				}
-			}
-		}
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users order by id", new RowMapper<User>() {
 
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setId(rs.getString("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+				return user;
+			}
+
+		});
 	}
 
 }
--- springbook/src/springbook/user/dao/UserDaoTest.java
+++ springbook/src/springbook/user/dao/UserDaoTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -89,6 +90,36 @@ public class UserDaoTest {
 
 		dao.get("unknown_id"); // UserDao에 손을 대지 않아서 test가 실패
 
+	}
+
+	@Test
+	public void getAll() throws SQLException, ClassNotFoundException {
+		dao.deleteAll();
+
+		dao.add(user1);
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+
+		dao.add(user2);
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));
+		checkSameUser(user2, users2.get(1));
+
+		dao.add(user3);
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user3, users3.get(0)); // user3의 id값이 알파벳순으로 가장 빠르므로
+		checkSameUser(user1, users3.get(1));
+		checkSameUser(user2, users3.get(2));
+
+	}
+
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
 	}
 
 	public static void main(String[] args) {
