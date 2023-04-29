9c1cabb3cbfac8e0c413e2d117af8059663a641c
nareun130 <nareun130@gmail.com>
2023-04-29 21:28:26 +0900

    [FEAT] : 다이나믹 프록시에 메소드 선별 부가기능 추가

--- springbook/src/springbook/learningtest/jdk/UppercaseHandler.java
+++ springbook/src/springbook/learningtest/jdk/UppercaseHandler.java
@@ -5,16 +5,20 @@ import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler{
 
-	Hello target;
+	Object target;
 	
 	//다이내믹 프록시로부터 전달받은 요청을 다시 타깃 오브젝트에 위임해야 하므로 타깃 오브젝트를 주입받아 둔다.
-	public UppercaseHandler(Hello target) {
+	public UppercaseHandler(Object target) {
 		this.target = target;
 	}
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		String ret = (String)method.invoke(target, args);
-		return ret.toUpperCase();// 부가기능 제공
+		Object ret = method.invoke(target, args);
+		if(ret instanceof String && method.getName().startsWith("say")) {
+			// 부가기능 제공 
+			return ((String)ret).toUpperCase();
+		}
+		return ret;
 	}
 
 }
