4d1f257eeb8583cac8e654460b91e80e17c1349f
nareun130 <nareun130@gmail.com>
2023-05-03 17:09:25 +0900

    [FEAT] : 포인트컷 표현식 테스트
    타깃 클래스 메소드 6개에 대해 포인트컷 선정여부를 검사하는 헬퍼메소드를 만들어서 포인트컷 표현식을 연습

--- springbook/src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ springbook/src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -31,4 +31,50 @@ public class PointcutExpressionTest {
 				&& pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));// 클래스 필터에서
 																											// 실패
 	}
+
+	// 포인트컷 표현식 테스트
+	@Test
+	public void pointcut() throws Exception {
+		targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+		targetClassPointcutMatches("execution(* hello(..))", true, true, false, false, false, false);
+		targetClassPointcutMatches("execution(* hello(String))", false, true, false, false, false, false);
+		targetClassPointcutMatches("execution(* meth*(..))", false, false, false, false, true, true);
+		targetClassPointcutMatches("execution(* *(int,int))", false, false, true, true, false, false);
+		targetClassPointcutMatches("execution(* *())", true, false, false, false, true, true);
+		targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.Target.*(..))", true, true,
+				true, true, true, false);
+		targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.*.*(..))", true, true, true,
+				true, true, true);
+		targetClassPointcutMatches("execution(* springbook..*.*(..))", true, true, true, true, true, true);
+		targetClassPointcutMatches("execution(* com..*.*(..))", false, false, false, false, false, false);
+		targetClassPointcutMatches("execution(* *..Target.*(..))", true, true, true, true, true, false);
+		targetClassPointcutMatches("execution(* *..Tar*.*(..))", true, true, true, true, true, false);
+		targetClassPointcutMatches("execution(* *..*get.*(..))", true, true, true, true, true, false);
+		targetClassPointcutMatches("execution(* *..B*.*(..))", false, false, false, false, false, true);
+		targetClassPointcutMatches("execution(* *..TargetInterface.*(..))", true, true, true, true, false, false);
+		targetClassPointcutMatches("execution(* *(..) throws Runtime*)", false, false, false, true, false, true);
+		targetClassPointcutMatches("execution(int *(..))", false, false, true, true, false, false);
+		targetClassPointcutMatches("execution(void *(..))", true, true, false, false, true, true);
+	}
+
+	// 포인트컷과 메소드를 비교해주는 테스트 헬퍼 메소드
+	public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName,
+			Class<?>... args) throws Exception {// 마지막 args는 파라미터
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(expression);
+
+		assertThat(pointcut.getClassFilter().matches(clazz)
+
+				&& pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null), is(expected));
+	}
+
+	public void targetClassPointcutMatches(String expression, boolean... expected) throws Exception {
+
+		pointcutMatches(expression, expected[0], Target.class, "hello");
+		pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+		pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+		pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+		pointcutMatches(expression, expected[4], Target.class, "method");
+		pointcutMatches(expression, expected[5], Bean.class, "method");
+	}
 }
