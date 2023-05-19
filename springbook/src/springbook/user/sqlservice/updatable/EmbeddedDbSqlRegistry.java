c6a8b9fc91d04d2e65fee5c899b517baefe24e93
nareun130 <nareun130@gmail.com>
2023-05-19 15:19:20 +0900

    [FEAT] : 다중 SQL에 대한 트랜잭션 테스트
    내장형 DB를 사용한 트랜잭션 테스트
    PlatformTransactionManager를 직접 사용해서 트랜잭션 처리 코드를 만드는 대신 트랜잭션 적용 코드에 템플릿/콜백 패턴을 적용한 TransactionTemplate을 씀.
    내장형 DB에 대한 트랜잭션 매니저는 공유할 필요가 없으므로 번거롭게 빈을 등록하는 대신 EmbeddedDbSqlRegistry 내부에서 직접 만들어서 사용함.

--- springbook/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ springbook/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -6,6 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import springbook.user.sqlservice.SqlNotFoundException;
 import springbook.user.sqlservice.SqlUpdateFailureException;
@@ -13,9 +17,13 @@ import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	SimpleJdbcTemplate jdbc;
+	// JdbcTemplate과 트랜잭션을 동기화해주는 트랜잭션 템플릿, 멀티스레드 환경에서 공유 가능
+	TransactionTemplate transactionTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		jdbc = new SimpleJdbcTemplate(dataSource);// DataSource를 DI받아 SimpleJdbcTemplate형태로 저장해두고 사용함.
+		// dataSource를 이용해 TransactionManager를 만들고 이를 이용해 TransactionTemplate를 생성
+		transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
 	}
 
 	@Override
@@ -41,11 +49,19 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 		}
 	}
 
+	// 익명 내부 클래스로 만들어지는 콜백 오브젝트 안에서 사용되는 것이라 final로 선언해줘야 함.
 	@Override
-	public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-		for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
-			updateSql(entry.getKey(), entry.getValue());
-		}
+	public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus arg0) {
+				for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+					updateSql(entry.getKey(), entry.getValue());
+				}
+			}
+		});
 	}
 
 }
--- springbook/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ springbook/src/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -2,15 +2,41 @@ package springbook.user.sqlservice.updatable;
 
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import static org.junit.Assert.fail;
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
-import org.junit.After;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.junit.After;
+import org.junit.Test;
+
+import springbook.user.sqlservice.SqlUpdateFailureException;
 import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 
 	EmbeddedDatabase db;
+
+	@Test
+	public void transactionalUpdate() {
+		// 초기 상태를 확인. -> 트랜잭션 롤백 후의 결과와 비교되서 이 테스트의 목적인 롤백 후의 상태는 처음과 동일하다는 것을 보여주려고 넣음.
+		checkFind("SQL1", "SQL2", "SQL3");
+		Map<String, String> sqlmap = new HashMap<String, String>();
+		sqlmap.put("KEY1", "modified1");
+		sqlmap.put("KEY9999!@#$", "Modified9999");
+
+		try {
+			sqlRegistry.updateSql(sqlmap);
+			fail();// 예외가 발생해서 catch로 넘어가지 않으면 뭔가 잘못된 것.-> fail()로 강제 실패 발생 시키고 원인을 찾아야 한다.
+		} catch (SqlUpdateFailureException e) {
+		}
+		// 첫 번째 SQL은 정상적으로 수정 했지만 트랜잭션이 롤백되기 때문에 다시 변경 이전 상태로 돌아와야 함.
+		// -> 트랜잭션이 적용되지 않는다면 변경된 채로 남아서 테스트는 실패할 것
+		checkFind("SQL1", "SQL2", "SQL3");
+
+	}
 
 	@Override
 	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
