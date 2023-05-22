f55402858243e88437e105e61cb91bc8c51f9246
nareun130 <nareun130@gmail.com>
2023-05-22 17:02:41 +0900

    [FEAT] : DB연결 정보 분리
    AppContext.class에 존재하던 dataSource의 db연결 정보를 database.properties파일로 분리시킴.

--- springbook/src/springbook/AppContext.java
+++ springbook/src/springbook/AppContext.java
@@ -1,57 +1,55 @@
 package springbook;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
-import org.hsqldb.Database;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.metadata.GenericTableMetaDataProvider;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.mysql.cj.jdbc.Driver;
 
-import springbook.user.dao.UserDao;
-import springbook.user.dao.UserDaoJdbc;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
-import springbook.user.service.UserServiceImpl;
 import springbook.user.service.UserServiceTest.TestUserService;
-import springbook.user.sqlservice.OxmSqlService;
-import springbook.user.sqlservice.SqlRegistry;
-import springbook.user.sqlservice.SqlService;
-import springbook.user.sqlservice.updatable.EmbeddedDbSqlRegistry;
 
 //DO 메타정보로 사용될 TestApplicationContext클래스
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.user")
 @Import(SqlServiceContext.class)
+@PropertySource("/database.properties")
 public class AppContext {
+
+	@Autowired
+	Environment env;
+
 	// DB 연결 & 트랜잭션
 	@Bean
 	public DataSource dataSource() {
-		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 
-		dataSource.setDriverClass(Driver.class);
-		dataSource.setUrl("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8");
-		dataSource.setUsername("spring");
-		dataSource.setPassword("book");
+		SimpleDriverDataSource ds = new SimpleDriverDataSource();
+		try {
+			ds.setDriverClass((Class<? extends java.sql.Driver>) Class.forName(env.getProperty("db.driverClass")));
 
-		return dataSource;
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		ds.setUrl(env.getProperty("db.url"));
+		ds.setUsername(env.getProperty("db.username"));
+		ds.setPassword(env.getProperty("db.password"));
+		return ds;
 	}
 
 	@Bean
